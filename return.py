edef33f05621ff583dc728c65ae9343fb4a4fe2a
CynthiaShiro <aimydewlux@gmail.com>
2020-08-13 22:11:08 +0300

    Final Push

--- return.py
+++ return.py
@@ -1,13 +1,13 @@
-def thisList(num1)
+def thisList(num1):
     for char in num1:
-        if char in num1:
+        if char in this_list:
             print("Its available!")
-                else:
-                    print("Error")
-this_list=["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+            print(this_list.index(char))
+        else:
+            print("Error")
+this_list=["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"]
 print(this_list)
 print(len(this_list))
-num2=input(this_list)
-print(this_list.index(char))
-thisList(num1)
+num2=input("Enter your word:")
+thisList(num2)
 
