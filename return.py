3e27beecf5f157e17c638040b7247d971d458cb8
CynthiaShiro <aimydewlux@gmail.com>
2020-08-13 21:07:29 +0300

    Final Push

--- return.py
+++ return.py
@@ -1,15 +1,12 @@
 def thisList(num1)
-    if x in num1:
-        print("Its available!")
-            else:
-                print("Error")
+    for char in num1:
+        if char in num1:
+            print("Its available!")
+                else:
+                    print("Error")
 this_list=["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 print(this_list)
 print(len(this_list))
+num2=print(this_list.index(char))
 thisList(num1)
 
-#def person(name):
-#    for x in name:
-#       print(x)
-#name=input("Please input your name:  ")
-#person(name)
