44d7a3ae7a9f552f21615433498c105ea9a905e7
CynthiaShiro <aimydewlux@gmail.com>
2020-08-13 20:14:17 +0300

    second commit

---
+++ return.py
@@ -0,0 +1,15 @@
+def thisList(num1)
+    if x in num1:
+        print("Its available!")
+            else:
+                print("Error")
+this_list=["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+print(this_list)
+print(len(this_list))
+thisList(num1)
+
+#def person(name):
+#    for x in name:
+#       print(x)
+#name=input("Please input your name:  ")
+#person(name)
